519355836fc224b28fe4db18c621580548789c7c
Alexander Kolesov <vvruspat@gmail.com>
2021-01-07 19:37:15 +0300

    add vk router

--- src/router.tsx
+++ src/router.tsx
@@ -3,31 +3,35 @@ import { createNavigator } from "@vkontakte/router";
 const routes = [
   {
     name: "projects",
+    updateUrl: false,
     children: [
       // Modals
-      { name: "project/edit", modal: true },
+      { name: "project/edit", modal: true, updateUrl: false },
       { name: "project/delete", modal: true, updateUrl: false },
       // Pages
-      { name: "project" },
+      { name: "project", updateUrl: false },
       {
         name: "project/task",
+        updateUrl: false,
         children: [
           // Modals
-          { name: "project/task/edit", modal: true },
+          { name: "project/task/edit", modal: true, updateUrl: false },
           { name: "project/task/delete", modal: true, updateUrl: false },
         ],
       },
       {
         name: "project/task/step",
+        updateUrl: false,
         children: [
           // Modals
-          { name: "project/task/step/edit", modal: true },
+          { name: "project/task/step/edit", modal: true, updateUrl: false },
           { name: "project/task/step/delete", modal: true, updateUrl: false },
         ],
       },
     ],
   },
 ];
+
 const config = {
   defaultRoute: "projects",
   rootPage: "projects",
